fe2ca191edb74af295f71566d99b8da2f5b07cf1
Peter Jang <peter.x.jang@gmail.com>
2021-06-01 21:15:05 -0500

    Add photos new/create actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="home">
+    <h1>New Photo</h1>
+    <div>
+      Name:
+      <input type="text" v-model="newPhoto.name" />
+      Width:
+      <input type="text" v-model="newPhoto.width" />
+      Height:
+      <input type="text" v-model="newPhoto.height" />
+      Url:
+      <input type="text" v-model="newPhoto.url" />
+      <button v-on:click="createPhoto()">Create Photo</button>
+    </div>
     <h1>All Photos</h1>
     <div v-for="photo in photos" v-bind:key="photo.id">
       <h2>{{ photo.name }}</h2>
@@ -19,6 +31,7 @@ export default {
   data: function () {
     return {
       photos: [],
+      newPhoto: {},
     };
   },
   created: function () {
@@ -30,6 +43,24 @@ export default {
         console.log("photos index", response);
         this.photos = response.data;
       });
+    },
+    createPhoto: function () {
+      var params = {
+        name: this.newPhoto.name,
+        width: this.newPhoto.width,
+        height: this.newPhoto.height,
+        url: this.newPhoto.url,
+      };
+      axios
+        .post("/photos", params)
+        .then((response) => {
+          console.log("photos create", response);
+          this.photos.push(response.data);
+          this.newPhoto = {};
+        })
+        .catch((error) => {
+          console.log("photos create error", error.response);
+        });
     },
   },
 };
