81d5b68437cddbfd56f58bf7a2d9e1836fe51ec5
Peter Jang <peter.x.jang@gmail.com>
2021-06-02 07:49:29 -0500

    Add photos index page

---
+++ src/views/PhotosIndex.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="photos-index">
+    <h1>All Photos</h1>
+    <div v-for="photo in photos" v-bind:key="photo.id">
+      <h2>{{ photo.name }}</h2>
+      <img v-bind:src="photo.url" v-bind:alt="photo.name" />
+      <p>Width: {{ photo.width }}</p>
+      <p>Height: {{ photo.height }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      photos: [],
+    };
+  },
+  created: function () {
+    this.indexPhotos();
+  },
+  methods: {
+    indexPhotos: function () {
+      axios.get("/photos").then((response) => {
+        console.log("photos index", response);
+        this.photos = response.data;
+      });
+    },
+  },
+};
+</script>
